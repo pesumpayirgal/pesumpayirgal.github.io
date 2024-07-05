18f6f8302c3d51158e77770f7d3ff777e5949a4b
raja1891 <raja@saasforest.com>
2024-07-06 01:41:08 +0530

    minor update

--- loadFooter.js
+++ loadFooter.js
@@ -3,6 +3,7 @@ function loadFooter() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
+            document.getElementById('current-year').innerText = new Date().getFullYear();
         })
         .catch(error => console.error('Error loading footer:', error));
 }
